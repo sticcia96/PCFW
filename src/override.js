9dfeff69b025e860701b1f3892fc163873190add
Thomas Andresen <thomas.andresen@earthgame.dk>
2013-03-11 22:17:56 +0100

    Auto detect override functions

--- src/override.js
+++ src/override.js
@@ -1,60 +1,62 @@
 PCFW.override = {
     init: function() {
-        PCFW.override.chatModels();
-        PCFW.override.API();
+        var objects = Object.keys(PCFW.override);
+        objects.splice(0,2);
+        for (var i in objects)
+            PCFW.override[objects[i]].init();
     },
     kill: function() {
-        Models.chat.chatCommand             = PCFW.__original.chatCommand;
-        Models.chat.addEventListener        = PCFW.__original.chatAddEvent;
-        Models.chat.removeEventListener     = PCFW.__original.chatRemoveEvent;
-        Models.chat.dispatchEvent           = PCFW.__original.chatDispatch;
-        API.delayDispatch                   = PCFW.__original.delayDispatch;
+        var objects = Object.keys(PCFW.override);
+        objects.splice(0,2);
+        for (var i in objects)
+            PCFW.override[objects[i]]..kill();
     },
-    chatModels: function() {
-        PCFW.__original.chatCommand         = Models.chat.chatCommand;
+    chatModels: {
+        init: function() {
+            PCFW.__original.chatCommand         = Models.chat.chatCommand;
 
-        PCFW.events.on("chatSoundUpdate",   $.proxy(Chat.onChatSoundUpdate,Chat));
-        PCFW.events.on("chatDelete",        $.proxy(Chat.onChatDelete,Chat));
-        PCFW.events.on("chatReceived",      $.proxy(Chat.onChatReceived,Chat));
-        PCFW.events.on("chatClear",         $.proxy(Chat.onChatClear,Chat));
-        PCFW.events.on("timestampUpdate",   $.proxy(Chat.onTimestampUpdate,Chat));
+            PCFW.events.on("chatSoundUpdate",   $.proxy(Chat.onChatSoundUpdate,Chat));
+            PCFW.events.on("chatDelete",        $.proxy(Chat.onChatDelete,Chat));
+            PCFW.events.on("chatReceived",      $.proxy(Chat.onChatReceived,Chat));
+            PCFW.events.on("chatClear",         $.proxy(Chat.onChatClear,Chat));
+            PCFW.events.on("timestampUpdate",   $.proxy(Chat.onTimestampUpdate,Chat));
 
-        Models.chat.chatCommand             = function(msg) {
-                                                if (PCFW.__original.chatCommand(msg) === true)
-                                                    return true;
-                                                if (msg.length === 0 || msg.substring(0,1) !== '/') return false;
-                                                var args = msg.split(' ');
-                                                PCFW.commands.execute(args.splice(0,1)[0].substring(1),args);
-                                            };
+            Models.chat.chatCommand             = function(msg) {
+                                                    if (PCFW.__original.chatCommand(msg) === true)
+                                                        return true;
+                                                    if (msg.length === 0 || msg.substring(0,1) !== '/') return false;
+                                                    var args = msg.split(' ');
+                                                    PCFW.commands.execute(args.splice(0,1)[0].substring(1),args);
+                                                };
 
-        PCFW.__original.chatAddEvent        = Models.chat.addEventListener;
-        Models.chat.addEventListener        = function (event,callback) {
-                                                PCFW.events.on(event,callback,PCFW.events.priority.SYSTEM);
-                                            };
-
-        PCFW.__original.chatRemoveEvent     = Models.chat.removeEventListener;
-        Models.chat.removeEventListener     = function (event,callback) {
-                                                PCFW.events.off(event,callback);
-                                            };
-
-        PCFW.__original.chatDispatch        = Models.chat.dispatchEvent;
-        Models.chat.dispatchEvent           = function (event,data) {
-                                                PCFW.events.emit(event,data);
-                                            };
+            PCFW.__original.chatDispatch        = Models.chat.dispatchEvent;
+            Models.chat.dispatchEvent           = function (event,data) {
+                                                    PCFW.events.emit(event,data);
+                                                };
+        },
+        kill: function() {
+            Models.chat.chatCommand             = PCFW.__original.chatCommand;
+            Models.chat.dispatchEvent           = PCFW.__original.chatDispatch;
+        }
     },
-    API: function() {
-        PCFW.__original.delayDispatch       = API.delayDispatch;
-        API.delayDispatch                   = function(event,data) {
-                                                if (!API.isReady)
-                                                    return;
-                                                PCFW.events.emit(event,data);
-                                                if (API.__events[event]) {
-                                                    setTimeout(function() {
-                                                        API.dispatchEvent(event,data);
-                                                        event = null;
-                                                        data = null;
-                                                    }, 1000);
-                                                }
-                                            };
+    API: {
+        init: function() {
+            PCFW.__original.delayDispatch       = API.delayDispatch;
+            API.delayDispatch                   = function(event,data) {
+                                                    if (!API.isReady)
+                                                        return;
+                                                    PCFW.events.emit(event,data);
+                                                    if (API.__events[event]) {
+                                                        setTimeout(function() {
+                                                            API.dispatchEvent(event,data);
+                                                            event = null;
+                                                            data = null;
+                                                        }, 1000);
+                                                    }
+                                                };
+        },
+        kill: function() {
+            API.delayDispatch                   = PCFW.__original.delayDispatch;
+        }
     }
 };
