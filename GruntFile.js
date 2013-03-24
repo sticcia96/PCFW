313c0dc829a05384464d167be47b6f45bf8551fb
Thomas Andresen <thomas.andresen@earthgame.dk>
2013-03-25 00:35:09 +0100

    Begin work on unit testing

---
+++ GruntFile.js
@@ -0,0 +1,59 @@
+module.exports = function( grunt ) {
+
+    "use strict";
+
+    var readOptionalJSON = function( filepath ) {
+            var data = {};
+            try {
+                data = grunt.file.readJSON( filepath );
+            } catch(e) {}
+            return data;
+        };
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        dst: readOptionalJSON("dist/.destination.json"),
+        testswarm: {
+            tests: (function() { return require('fs').readdirSync('./test/unit/'); })()
+        },
+    });
+
+    grunt.registerTask("testswarm", function( commit, configFile ) {
+        var jobName,
+            testswarm = require("testswarm"),
+            testUrls = [],
+            pull = /PR-(\d+)/.exec(commit),
+            config = grunt.file.readJSON(configFile),
+            tests = grunt.config([ this.name, "tests" ]);
+
+        if ( pull ) {
+            jobName = "PCFW pull <a href='https://github.com/TATDK/PCFW/pull/" +
+                pull[ 1 ] + "'>#" + pull[ 1 ] + "</a>";
+        } else {
+            jobName = "PCFW commit #<a href='https://github.com/TATDK/PCFW/commit/" +
+                commit + "'>" + commit.substr( 0, 10 ) + "</a>";
+        }
+
+        tests.forEach(function( test ) {
+            testUrls.push( config.testUrl + commit + "/test/index.html?module=" + test );
+        });
+
+        testswarm({
+            url: config.swarmUrl,
+            pollInterval: 10000,
+            timeout: 1000 * 60 * 30,
+            done: this.async()
+        }, {
+            authUsername: config.authUsername,
+            authToken: config.authToken,
+            jobName: jobName,
+            runMax: config.runMax,
+            "runNames[]": tests,
+            "runUrls[]": testUrls,
+            "browserSets[]": "PCFW"
+        });
+    });
+
+    // Default grunt
+    grunt.registerTask( "default", [] );
+};
