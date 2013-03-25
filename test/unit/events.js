d7830d2ece5a247d302bc4a312d46ea3e28bcab7
Thomas Andresen <thomas.andresen@earthgame.dk>
2013-03-25 13:18:52 +0100

    Event unittest update

--- test/unit/events.js
+++ test/unit/events.js
@@ -4,8 +4,6 @@ test("add/remove event",function() {
     var callback = function(){};
     strictEqual(PCFW.events.on(),false,"check for no parameters when adding");
     strictEqual(PCFW.events.on("test",callback),true,"true on added event");
-    strictEqual(PCFW.events.__events.test.length,1,"event added to event object");
     strictEqual(PCFW.events.off(),false,"check for no parameters when removing");
     strictEqual(PCFW.events.off("test",callback),true,"true on removed event");
-    strictEqual(PCFW.events.__events.test.length,0,"event removed to event object");
 });
